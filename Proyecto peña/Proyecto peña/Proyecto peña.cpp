6b570684a3c3d824611cfe6f420753ce1bc52389
GeralDuran <durangeraldine05@gmail.com>
2023-12-08 12:14:31 -0600

    realizamos el cambio a minusculas y los caracteres que son permitidos

--- Proyecto peña/Proyecto peña/Proyecto peña.cpp
+++ Proyecto peña/Proyecto peña/Proyecto peña.cpp
@@ -1,19 +1,96 @@
-// Proyecto peña.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+/*****************************************************************************************************************
+	UNIVERSIDAD NACIONAL AUTONOMA DE MEXICO
+	FACULTAD DE ESTUDIOS SUPERIORES -ARAGON-
 
-#include <iostream>
+	Computadoras y programacion.
+	(c) Granados Duran Evelin Geraldine 321315153
 
-int main()
+	Quiso decir: Programa principal de la aplicacion de la distancia de Levenstein.
+
+******************************************************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define TAMTOKEN 1000
+#define MAX_ELEMENTOS 70000
+#define MAX_SUGERIDAS 100
+
+
+//Funciones publicas del proyecto
+/*****************************************************************************************************************
+	DICCIONARIO: Esta funcion crea el diccionario completo
+	char *	szNombre				:	Nombre del archivo de donde se sacaran las palabras del diccionario
+	char	szPalabras[][TAMTOKEN]	:	Arreglo con las palabras completas del diccionario
+	int		iEstadisticas[]			:	Arreglo con el numero de veces que aparecen las palabras en el diccionario
+	int &	iNumElementos			:	Numero de elementos en el diccionario
+******************************************************************************************************************/
+
+// funciones del diccionario 
+
+// permitir solo letras y numeros 
+int esCaracterPermitido(char c)
 {
-    std::cout << "Hello World!\n";
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'));
+}
+//eliminar signos de puntuacion y cambiarlos por un espacio
+void eliminarPuntuacion(char* cadena)
+{
+	int longitud = strlen(cadena);
+	for (int i = 0; i < longitud; i++)
+	{
+		if (!esCaracterPermitido(cadena[i]))
+		{
+			cadena[i] = ' ';
+		}
+	}
+}
+//convertimos todas las cadenas en minuscula
+void convertirMinusculas(char* cadena)
+{
+	int longitud = strlen(cadena);
+	for (int i = 0; i < longitud; i++)
+	{
+		cadena[i] = tolower(cadena[i]);
+	}
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+
+/*****************************************************************************************************************
+	ClonaPalabras: toma una palabra y obtiene todas las combinaciones y permutaciones requeridas por el metodo
+	char *	szPalabraLeida,						// Palabra a clonar
+	char	szPalabrasSugeridas[][TAMTOKEN], 	//Lista de palabras clonadas
+	int &	iNumSugeridas)						//Numero de elementos en la lista
+******************************************************************************************************************/
+
+/*****************************************************************************************************************
+	ListaCandidatas: Esta funcion recupera desde el diccionario las palabras validas y su peso
+	Regresa las palabras ordenadas por su peso
+	char	szPalabrasSugeridas[][TAMTOKEN],	//Lista de palabras clonadas
+	int		iNumSugeridas,						//Lista de palabras clonadas
+	char	szPalabras[][TAMTOKEN],				//Lista de palabras del diccionario
+	int		iEstadisticas[],					//Lista de las frecuencias de las palabras
+	int		iNumElementos,						//Numero de elementos en el diccionario
+	char	szListaFinal[][TAMTOKEN],			//Lista final de palabras a sugerir
+	int		iPeso[],							//Peso de las palabras en la lista final
+	int &	iNumLista)							//Numero de elementos en la szListaFinal
+******************************************************************************************************************/
+
+
+int main() {
+	char szPalabras[MAX_ELEMENTOS][TAMTOKEN];
+	int iEstadisticas[MAX_ELEMENTOS];
+	int iNumElementos = 0;
+	char nombreArchivo[] = fpDicc
+
+		Diccionario(nombreArchivo, szPalabras, iEstadisticas, &iNumElementos);
+
+	// Imprime el diccionario ordenado
+	printf("Diccionario ordenado alfabéticamente:\n");
+	for (int i = 0; i < iNumElementos; i++) {
+		printf("%s - Frecuencia: %d\n", szPalabras[i], iEstadisticas[i]);
+	}
+
+	
